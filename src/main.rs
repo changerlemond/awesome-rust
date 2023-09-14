343387927826ecf77c38a878469c52477a1250d6
changerlemond <dev.jungclaire@gmail.com>
2023-09-15 03:35:50 +0900

    apply: actix appState test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use actix_web::{App, get, HttpResponse, HttpServer, post, Responder, web};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
-}
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello World!")
+// }
 
 #[post("/echo")]
 async fn echo(request_body: String) -> impl Responder {
@@ -14,13 +14,24 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web Test!")
+            }))
+            .service(index)
     })
         .bind(("127.0.0.1", 8080))?
         .run()
